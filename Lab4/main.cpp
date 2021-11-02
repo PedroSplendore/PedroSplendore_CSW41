c3b41fc6f0815a51ee52ce2aa2dd9abe4e8215c1
Peeteer2 <splendore@alunos.utfpr.edu.br>
2021-11-02 19:58:35 -0300

    problemas com UARTConfigSetExpClk

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -17,8 +17,61 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/systick.h"
 #include "driverlib/pin_map.h"
+#include "driverlib/interrupt.h"
+#include "inc/hw_ints.h"
 
 using namespace std;
+
+//****************************************************************************
+//
+// System clock rate in Hz.
+//
+//****************************************************************************
+uint32_t SysClock;
+
+void
+UARTIntHandler(void)
+{
+    uint32_t ui32Status;
+
+    //
+    // Get the interrrupt status.
+    //
+    ui32Status = UARTIntStatus(UART0_BASE, true);
+
+    //
+    // Clear the asserted interrupts.
+    //
+    UARTIntClear(UART0_BASE, ui32Status);
+
+    //
+    // Loop while there are characters in the receive FIFO.
+    //
+    while(UARTCharsAvail(UART0_BASE))
+    {
+        //
+        // Read the next character from the UART and write it back to the UART.
+        //
+        UARTCharPutNonBlocking(UART0_BASE,
+                                   UARTCharGetNonBlocking(UART0_BASE));
+
+        //
+        // Blink the LED to show a character transfer is occuring.
+        //
+        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
+
+        //
+        // Delay for 1 millisecond.  Each SysCtlDelay is about 3 clocks.
+        //
+        SysCtlDelay(SysClock / (1000 * 3));
+
+        //
+        // Turn off the LED
+        //
+        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0);
+    }
+}
+
 
 void UARTInit(void){
   // Enable the GPIO Peripheral used by the UART.
@@ -35,18 +88,27 @@ void UARTInit(void){
   GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
   cout <<"a";
-  UARTConfigSetExpClk(UART0_BASE, 40000000, 9600, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+  UARTConfigSetExpClk(UART0_BASE, SysClock, 115200,(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
   cout <<"b";
   UARTEnable(UART0_BASE);
   cout <<"a";
   UARTCharPut(UART0_BASE, 'a');
   cout <<"a";
+  
 } // UARTInit
 
 int main(){
+  //clock a 120MHz
+  SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
+                                             SYSCTL_OSC_MAIN |
+                                             SYSCTL_USE_PLL |
+                                             SYSCTL_CFG_VCO_240), 120000000);
+    //
+    // Enable processor interrupts.
+    //
+  IntMasterEnable(); 
   cout <<"a";
- // SysCtlDelay(120000000);
-  uint32_t teste1 = UARTClockSourceGet(UART0_BASE);
-  uint32_t systemClock = SysCtlClockFreqSet(SYSCTL_OSC_INT | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_320,40000000);
+  IntEnable(INT_UART0);
+  UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
   UARTInit();
 }

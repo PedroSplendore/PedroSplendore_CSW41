ffe6daaaa11666e491dcbd9b2c581b00220527ae
Peeteer2 <splendore@alunos.utfpr.edu.br>
2021-11-02 11:50:24 -0300

    lab4 incomplete with bugs

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -3,12 +3,13 @@
 // Configure the UART and its pins.  This must be called before UARTprintf().
 //
 //*****************************************************************************
+
 #include <stdint.h>
 #include <stdbool.h>
 #include <iostream>
 #include <stdio.h>
 #include "utils/uartstdio.h"
-#include "system_TM4C1294.h"
+//#include "system_TM4C1294.h"
 #include "inc/hw_memmap.h"
 // includes da biblioteca driverlib
 #include "driverlib/gpio.h"
@@ -19,10 +20,7 @@
 
 using namespace std;
 
-extern void UARTStdioIntHandler(void);
-
 void UARTInit(void){
-  
   // Enable the GPIO Peripheral used by the UART.
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA));
@@ -36,11 +34,19 @@ void UARTInit(void){
   GPIOPinConfigure(GPIO_PA1_U0TX);
   GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
-  // Initialize the UART for console I/O.
-  UARTStdioConfig(0, 9600, SystemCoreClock);
+  cout <<"a";
+  UARTConfigSetExpClk(UART0_BASE, 40000000, 9600, (UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+  cout <<"b";
+  UARTEnable(UART0_BASE);
+  cout <<"a";
+  UARTCharPut(UART0_BASE, 'a');
+  cout <<"a";
 } // UARTInit
 
 int main(){
-  UARTInit();
   cout <<"a";
+ // SysCtlDelay(120000000);
+  uint32_t teste1 = UARTClockSourceGet(UART0_BASE);
+  uint32_t systemClock = SysCtlClockFreqSet(SYSCTL_OSC_INT | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_320,40000000);
+  UARTInit();
 }

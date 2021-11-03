97a8dccc9ce0d793776dd4d4337778bebe1e51df
Peeteer2 <splendore@alunos.utfpr.edu.br>
2021-11-03 08:08:04 -0300

    minor changes

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -28,49 +28,7 @@ using namespace std;
 //
 //****************************************************************************
 uint32_t SysClock;
-
-void
-UARTIntHandler(void)
-{
-    uint32_t ui32Status;
-
-    //
-    // Get the interrrupt status.
-    //
-    ui32Status = UARTIntStatus(UART0_BASE, true);
-
-    //
-    // Clear the asserted interrupts.
-    //
-    UARTIntClear(UART0_BASE, ui32Status);
-
-    //
-    // Loop while there are characters in the receive FIFO.
-    //
-    while(UARTCharsAvail(UART0_BASE))
-    {
-        //
-        // Read the next character from the UART and write it back to the UART.
-        //
-        UARTCharPutNonBlocking(UART0_BASE,
-                                   UARTCharGetNonBlocking(UART0_BASE));
-
-        //
-        // Blink the LED to show a character transfer is occuring.
-        //
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, GPIO_PIN_0);
-
-        //
-        // Delay for 1 millisecond.  Each SysCtlDelay is about 3 clocks.
-        //
-        SysCtlDelay(SysClock / (1000 * 3));
-
-        //
-        // Turn off the LED
-        //
-        GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_0, 0);
-    }
-}
+uint32_t dataUART[16] = {};
 
 
 void UARTInit(void){
@@ -81,21 +39,50 @@ void UARTInit(void){
   // Enable UART0
   SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0));
-
+  //
+  // Enable processor interrupts.
+  //
+  IntMasterEnable();
   // Configure GPIO Pins for UART mode.
   GPIOPinConfigure(GPIO_PA0_U0RX);
   GPIOPinConfigure(GPIO_PA1_U0TX);
   GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
 
   cout <<"a";
-  UARTConfigSetExpClk(UART0_BASE, SysClock, 115200,(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+  UARTConfigSetExpClk(UART0_BASE, SysClock, 9600,(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
   cout <<"b";
   UARTEnable(UART0_BASE);
+  IntEnable(INT_UART0);
+  UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
+  
   cout <<"a";
   UARTCharPut(UART0_BASE, 'a');
   cout <<"a";
   
+  
 } // UARTInit
+
+void UARTSend(void)
+{
+  for(uint32_t n=0; n==(sizeof(dataUART)/sizeof(uint32_t));n++){
+    UARTCharPut(UART0_BASE, dataUART[n]);
+  }
+}
+
+void UARTIntHandler(void){
+   uint32_t n=0;
+   
+   uint32_t ui32Status;
+   ui32Status = UARTIntStatus(UART0_BASE, true);
+   UARTIntClear(UART0_BASE, ui32Status);
+   
+   while(UARTCharsAvail(UART0_BASE))
+    {
+      dataUART[n] = UARTCharGetNonBlocking(UART0_BASE);
+      n++;
+      SysCtlDelay(SysClock / (1000 * 3));
+    }
+}
 
 int main(){
   //clock a 120MHz
@@ -103,12 +90,20 @@ int main(){
                                              SYSCTL_OSC_MAIN |
                                              SYSCTL_USE_PLL |
                                              SYSCTL_CFG_VCO_240), 120000000);
-    //
-    // Enable processor interrupts.
-    //
-  IntMasterEnable(); 
-  cout <<"a";
-  IntEnable(INT_UART0);
-  UARTIntEnable(UART0_BASE, UART_INT_RX | UART_INT_RT);
   UARTInit();
+  
+    //
+  // Prompt for text to be entered.
+  //
+  UARTSend();
+
+  //
+  // Loop forever echoing data through the UART.
+  //
+  while(1)
+  {
+    //UARTReceive();
+    UARTSend();
+  }
+
 }
